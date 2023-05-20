8b696756e0c4d7cd7108b493fb2a960cc7cfa64b
jeevan rawal <rawaljeevan123@gmail.com>
2023-05-20 10:15:10 +0545

    teacherSubject_seeder added

---
+++ database/seeders/teacher_subject.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Subject;
+use App\Models\Teacher;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class teacher_subject extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $subject_count= Subject::count();
+        $teachers= Teacher::all();
+        //dd($teachers);
+       foreach($teachers as $teacher){
+        $teacher->subject()->attach(rand(1,$subject_count));
+       }
+    }
+}
